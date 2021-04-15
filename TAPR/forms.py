1b54ce73a2156c6f439355bfee9ead416cbd7d9d
Jacob Ivins <ivinsjn@cardiff.ac.uk>
2021-04-15 15:20:20 +0100

    initial commit

--- TAPR/forms.py
+++ TAPR/forms.py
@@ -6,7 +6,7 @@ from TAPR.models import *
 class RegistrationForm(FlaskForm):
     id = StringField('ID', validators=[DataRequired(), Length(min=3, max=15, message="Username should be 3 to 15 characters long.")],render_kw={"placeholder":"3 to 15 characters long"})
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(),Regexp('^(?=.*\d).{8,16}$',message="Your password should be between 8 and 16 characters long.")], render_kw={"placeholder":"8 to 16 characters long"})
+    password = PasswordField('Password',validators=[DataRequired(),Regexp('^.{6,8}$',message='Password must be between 6 and 8 characters long.')])
     confirm_password=PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password',message="Two Passwords are not the same.")])
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
@@ -27,3 +27,13 @@ class LoginForm(FlaskForm):
     id = StringField('ID', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+class TeamAllocation(FlaskForm):
+    assessment = IntegerField('Assessment ID')
+    team_size = RadioField('Specify team size range by choosing one of the following:', choices=[(5, '5-6 students per group'), (6, '6-7 students per group')])
+    prior_programming = BooleanField('Where possible include at least one student with prior programming experience for each team:')
+    native_speaker = BooleanField('Where possible include at least one English native speaker for each team:')
+    prev_degree = BooleanField('Where possible include at least one BA, BSc, BEng and LLB graduate for each team:')
+    submit = SubmitField('Submit')
+
+
--- TAPR/routes.py
+++ TAPR/routes.py
@@ -52,3 +52,39 @@ def register():
     # else:
     #     flash_errors(form)
     return render_template('register.html', title='Register', form=form)
+
+
+
+
+
+
+@app.route("/")
+@app.route("/team_allocation", methods=['GET', 'POST'])
+def team_allocation():
+    form = TeamAllocation()
+    if form.validate_on_submit():
+        if len(Assessment.query.filter_by(id=form.assessment.data).first().student_team_list) > 0:
+            flash("Teams already allocated!")
+            return redirect(url_for('home'))
+        assessment = Assessment.query.filter_by(id=form.assessment.data).first()
+        students = User.query.filter_by(assessment_id=assessment.id).all()
+        min_team_size = int(form.team_size.data)
+        team_count = len(students) // min_team_size
+
+        #Initialize teams
+        for team_id in range(1, team_count+1):
+            team = Team(id = team_id, assessment_id = assessment.id)
+            db.session.add(team)
+        teams = assessment.student_team_list
+
+        #Allocate students to teams
+        if form.native_speaker.data: addNativeSpeakers(teams, students)
+        if form.prior_programming.data: addPriorProgrammers(teams, students)
+        if form.prev_degree.data: addPreviousDegrees(teams, students, min_team_size)
+        allocateStudents(teams, students, min_team_size) #Allocate any students not allocated
+
+        db.session.commit()
+        flash("Teams have been allocated!")
+        return redirect(url_for('home'))
+        
+    return render_template('team_allocation.html', title = "Team Allocation", form=form)
